e43113d7a3bb9c749c4e4c2ab4d1356769d1fd7c
David Juarez <jdavidjuarez95@gmail.com>
2022-07-06 00:08:34 -0600

    STABLE - Updated gradle version, added more ui elemets to LoginActivity.kt, and updated .gitignore
    Signed-off-by: David Juarez <jdavidjuarez95@gmail.com>

--- app/src/main/java/com/example/droidmoviesdb/LoginActivity.kt
+++ app/src/main/java/com/example/droidmoviesdb/LoginActivity.kt
@@ -3,11 +3,17 @@ package com.example.droidmoviesdb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.droidmoviesdb.ui.theme.DroidMoviesDBTheme
 
 class LoginActivity : ComponentActivity() {
@@ -20,7 +26,7 @@ class LoginActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LoginForm()
                 }
             }
         }
@@ -30,18 +36,51 @@ class LoginActivity : ComponentActivity() {
 @Composable
 fun Title(
     title: String
-){
+) {
     Text(
         title,
         style = MaterialTheme.typography.headlineLarge
     )
 }
 
+@Composable
+fun UsernameTextField() {
+    var usernameText by rememberSaveable { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = usernameText,
+        onValueChange = { usernameText = it },
+        label = { Text("Username") },
+        leadingIcon = { Icon(Icons.Default.Person, contentDescription = "") }
+    )
+}
+
+@Composable
+fun PasswordTextField() {
+    var passwordText by rememberSaveable { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = passwordText,
+        onValueChange = { passwordText = it },
+        placeholder = { Text("Password") },
+        leadingIcon = { Icon(Icons.Default.Lock, contentDescription = "") }
+    )
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginForm(){
-    Card {
-        Title("Login CARD!")
+fun LoginForm() {
+    Card(
+
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Title(stringResource(R.string.login_Title))
+            UsernameTextField()
+            Spacer(modifier = Modifier.height(16.dp))
+            PasswordTextField()
+        }
     }
 }
 
@@ -50,10 +89,10 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(widthDp = 360, heightDp = 640, showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Preview(widthDp = 360, heightDp = 640, showBackground = true, backgroundColor = 0x9ea39f)
 @Composable
 fun DefaultPreview() {
     DroidMoviesDBTheme {
-        Title("Android")
+        LoginForm()
     }
 }
