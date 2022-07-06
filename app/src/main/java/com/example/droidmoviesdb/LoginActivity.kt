304a49de584a9d09862daae0373bcec69b8b35b7
David Juarez <jdavidjuarez95@gmail.com>
2022-07-05 23:08:22 -0600

    STABLE - Started to build LoginActivity.kt and updated all dependencies
    Signed-off-by: David Juarez <jdavidjuarez95@gmail.com>

--- app/src/main/java/com/example/droidmoviesdb/LoginActivity.kt
+++ app/src/main/java/com/example/droidmoviesdb/LoginActivity.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.droidmoviesdb.ui.theme.DroidMoviesDBTheme
 
-class MainActivity : ComponentActivity() {
+class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,14 +28,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun Title(
+    title: String
+){
+    Text(
+        title,
+        style = MaterialTheme.typography.headlineLarge
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginForm(){
+    Card {
+        Title("Login CARD!")
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview(widthDp = 360, heightDp = 640, showBackground = true, backgroundColor = 0xFFF0EAE2)
 @Composable
 fun DefaultPreview() {
     DroidMoviesDBTheme {
-        Greeting("Android")
+        Title("Android")
     }
 }
