b75a45c9478b425304b28eb11cab30e402b842ff
David Juarez <jdavidjuarez95@gmail.com>
2022-07-06 18:43:15 -0600

    STABLE - added firebase to project
    Signed-off-by: David Juarez <jdavidjuarez95@gmail.com>

--- app/src/main/java/com/example/droidmoviesdb/LoginActivity.kt
+++ app/src/main/java/com/example/droidmoviesdb/LoginActivity.kt
@@ -23,7 +23,7 @@ class LoginActivity : ComponentActivity() {
             DroidMoviesDBTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+//                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     LoginForm()
