7173816dc05acb14e98833ae00262d253d2b7b5e
David Juarez <jdavidjuarez95@gmail.com>
2022-07-17 00:07:55 -0600

    STABLE - started building search results ui

--- app/src/main/java/com/example/droidmoviesdb/MainActivity.kt
+++ app/src/main/java/com/example/droidmoviesdb/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import com.example.droidmoviesdb.ui.SingleSearchResult
 import com.example.droidmoviesdb.ui.theme.DroidMoviesDBTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,6 +51,27 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
+    fun SearchResults() {
+        LazyColumn {
+            // TODO Add iterator over List of search results
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun SingleResult(singleSearchResult: SingleSearchResult) {
+        Card {
+            Column {
+                Text(
+                    singleSearchResult.title,
+                    style = MaterialTheme.typography.headlineLarge
+                )
+            }
+
+        }
+    }
+
+    @Composable
     fun TopAppBar() {
         SmallTopAppBar(
             title = {
@@ -75,6 +100,20 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    val sampleSingleResult = SingleSearchResult(
+        "Split",
+        "2016",
+        "tt4972582",
+        "movie",
+        "https://m.media-amazon.com/images/M/MV5BZTJiNGM2NjItNDRiYy00ZjY0LTgwNTItZDBmZGRlODQ4YThkL2ltYWdlXkEyXkFqcGdeQXVyMjY5ODI4NDk@._V1_SX300.jpg"
+    )
+
+    @Preview(showBackground = true, showSystemUi = true)
+    @Composable
+    fun SingleResultPreview() {
+        SingleResult(singleSearchResult = sampleSingleResult)
     }
 
 }
---
+++ app/src/main/java/com/example/droidmoviesdb/ui/SingleSearchResult.kt
@@ -0,0 +1,9 @@
+package com.example.droidmoviesdb.ui
+
+data class SingleSearchResult(
+    val title: String,
+    val year: String,
+    val imdbId: String,
+    val type: String,
+    val poster: String
+)
